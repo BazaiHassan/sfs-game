e6bdcc8d368dc6b19a7a245d59314fabb4228524
bazai hassan <bazaee.hassan@gmail.com>
2024-09-30 23:51:39 +0330

    initial and Assets manager class

---
+++ android/src/main/java/io/github/hbazai/fighter/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package io.github.hbazai.fighter.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.github.hbazai.fighter.FighterGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new FighterGame(), configuration);
+    }
+}
---
+++ core/src/main/java/io/github/hbazai/fighter/resources/Assets.java
@@ -0,0 +1,26 @@
+package io.github.hbazai.fighter.resources;
+
+import com.badlogic.gdx.assets.AssetManager;
+
+public class Assets {
+    // Asset manager
+    public final AssetManager manager = new AssetManager();
+
+    // Gameplay Assets
+    public static final String BACKGROUND_TEXTURE = "textures/Background.png";
+    public static final String FRONT_REPOES_TEXTURE = "textures/FrontRopes.png";
+    public static final String GAMEPLAY_BUTTONS_ATLAS = "textures/GameplayButtons.atlas";
+    public static final String BLOOD_ATLAS = "textures/Blood.atlas";
+
+    public static final String IDLE_SPRITE_SHEET ="sprites/IdleSpriteSheet.png";
+    public static final String WALK_SPRITE_SHEET ="sprites/WalkSpriteSheet.png";
+    public static final String PUNCH_SPRITE_SHEET ="sprites/PunchSpriteSheet.png";
+    public static final String KICK_SPRITE_SHEET = "sprites/KickSpriteSheet.png";
+    public static final String HURT_SPRITE_SHEET = "sprites/HurtSpriteSheet.png";
+    public static final String BLOCK_SPRITE_SHEET = "sprites/BlockSpriteSheet.png";
+    public static final String WIN_SPRITE_SHEET = "sprites/WinSpriteSheet.png";
+    public static final String LOSE_SPRITE_SHEET = "sprites/LoseSpriteSheet.png";
+
+
+
+}
